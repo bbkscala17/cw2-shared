e6bd476d986d55438c95bed57b603ac8fe9f9e21
dwatso09 <bbkscala17@users.noreply.github.com>
2017-03-03 19:02:21 +0000

    Bytcodeffactory intitial

---
+++ src/main/scala/bc/ByteCodeFactoryImpl.scala
@@ -0,0 +1,20 @@
+package bc
+
+class ByteCodeFactoryImpl extends ByteCodeFactory{
+  /**
+    * Returns a [[ByteCode]].
+    *
+    * This method creates a new [[ByteCode]] object given the `byte`
+    * that corresponds to the bytecode (see [[ByteCodeValues]]. If
+    * the bytecode requires arguments then an optional integer
+    * argument is provided.
+    *
+    * This method should throw an [[InvalidBytecodeException]] if the
+    * given bytecode value is unknown.
+    *
+    * @param byte the byte code of a bytecode
+    * @param args an optional integer argument (depends on bytecode)
+    * @return a new bytecode object
+    */
+  override def make(byte: Byte, args: Int*): ByteCode = ???
+}
