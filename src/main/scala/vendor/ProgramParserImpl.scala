198453df8008af41bba2374f7afe0af78bfdbe5e
diegoromero <diego.romero@dataffirm.com>
2017-03-03 14:59:53 +0000

    stable first try

--- src/main/scala/vendor/ProgramParserImpl.scala
+++ src/main/scala/vendor/ProgramParserImpl.scala
@@ -1,4 +1,5 @@
 package vendor
+import scala.io.Source
 
 /**
   * Created by diegoromero on 03/03/2017.
@@ -13,8 +14,18 @@ class ProgramParserImpl extends ProgramParser {
     * @return an instruction list
     */
   override def parse(file: String): InstructionList = {
-
-    new Vector[Instruction]
+    val lines = Source.fromFile(file).getLines
+    val instructionList = Vector()
+    for (line <- lines) {
+      val fields = line.split(" ")
+      if (fields.nonEmpty) {
+        println(line)
+      }
+    }
+    println(instructionList)
+    val vector: Vector[Int] = Vector(1, 2, 3)
+    val inst: Instruction = new Instruction("iconst", vector)
+    Vector(inst, inst)
   }
 
   /**
@@ -24,27 +35,5 @@ class ProgramParserImpl extends ProgramParser {
     * @param string the string to parse
     * @return an instruction list
     */
-  override def parseString(string: String): InstructionList = {
-    new Vector[Instruction]
-  }
-}
-
-object Driver extends App{
-  val programParser = new ProgramParserImpl
-
-  // println(programParser.parse("diego"))
-    import scala.io.Source
-    val lines = Source.fromFile(file).getLines
-    val instructionList = Vector()
-    for (line <- lines) {
-      val fields = line.split(" ")
-      if (fields.nonEmpty) {
-        addToVector(line, instructionList)
-        println(line)
-      }
-    }
-    println(instructionList)
-    val vector: Vector[Int] = Vector(1, 2, 3)
-    val inst: Instruction = new Instruction("iconst", vector)
-    Vector(inst, inst)
+  override def parseString(string: String): InstructionList = Vector()
 }
