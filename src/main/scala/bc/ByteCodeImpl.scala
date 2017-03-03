37a19276c7bdc048a3cf0c71cd0c0bf0e19d4929
dwatso09 <bbkscala17@users.noreply.github.com>
2017-03-03 18:14:46 +0000

    bytcode initial

---
+++ src/main/scala/bc/ByteCodeImpl.scala
@@ -0,0 +1,15 @@
+package bc
+
+import vm.VirtualMachine
+
+class ByteCodeImpl extends ByteCode{
+  override val code: Byte = _
+
+  /**
+    * Returns a new [[VirtualMachine]] after executing this bytecode operation.
+    *
+    * @param vm the initial virtual machine
+    * @return a new virtual machine
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = ???
+}
