6f4a9940385a02f8c900f4db04ca1132cc79bb7c
diegoromero <diego.romero@dataffirm.com>
2017-03-03 14:28:08 +0000

    starting implementation of program parser

---
+++ src/main/scala/vendor/ProgramParserImpl.scala
@@ -0,0 +1,24 @@
+package vendor
+
+/**
+  * Created by diegoromero on 03/03/2017.
+  */
+class ProgramParserImpl extends ProgramParser {
+  /**
+    * Parses a file representation of a bytecode program
+    * into an `InstructionList`.
+    *
+    * @param file the file to parse
+    * @return an instruction list
+    */
+  override def parse(file: String): InstructionList = ???
+
+  /**
+    * Parses a string representation of a bytecode program
+    * into an `InstructionList`.
+    *
+    * @param string the string to parse
+    * @return an instruction list
+    */
+  override def parseString(string: String): InstructionList = ???
+}
