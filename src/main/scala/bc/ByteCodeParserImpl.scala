49882e0ac19fd6dbf209df50903dfeb7adcd2a5b
dwatso09 <bbkscala17@users.noreply.github.com>
2017-03-05 11:27:17 +0000

    adding comment to test

--- src/main/scala/bc/ByteCodeParserImpl.scala
+++ src/main/scala/bc/ByteCodeParserImpl.scala
@@ -3,9 +3,10 @@ package bc
 class ByteCodeParserImpl extends ByteCodeParser{
   /**
     * Parses a vector of `Byte` into a vector of `ByteCode`.
+    * Uses ByteCodeFactory to construct ByteCode objects from Byte
     *
     * You should use [[ByteCodeValues.bytecode]] to help translate
-    * the individual `Byte`s into a correponding [[ByteCode]].
+    * the individual `Byte`s into a corresponding [[ByteCode]].
     *
     * @param bc a vector of bytes representing bytecodes
     * @return a vector of `ByteCode` objects
